8688c7a2c13e65aa9fcbbe0861bbac30aa7f554d
Mohammed Salim <mohammed.23s@outlook.com>
2025-06-15 21:24:32 +0400

    The Spring project has been added with two new APIs

--- src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
+++ src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
@@ -2,9 +2,11 @@ package guru.springframework.springaiintro.controllers;
 
 
 import guru.springframework.springaiintro.model.Answer;
+import guru.springframework.springaiintro.model.GetCapitalRequest;
 import guru.springframework.springaiintro.model.Question;
 import guru.springframework.springaiintro.services.OpenAIService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +17,13 @@ public class QuestionController {
         this.openAIService = openAIService;
     }
 
+    @PostMapping("/capital")
+    public Answer getCapital(@RequestBody GetCapitalRequest getCapitalRequest) {
+        return openAIService.getCapital(getCapitalRequest);
+    }
 
-    @PostMapping
-    public Answer askQuestion(Question question) {
+    @PostMapping("/ask")
+    public Answer askQuestion(@RequestBody Question question) {
         return openAIService.getAnswer(question);
     }
 
